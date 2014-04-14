23c25ec85e66638d3b262f120cdaceeb82514710
rpawlik <ramsey.pawlik@rackspace.com>
2014-04-14 04:11:45 +0000

    added additional tests

--- rpc-ssl-patch.sh
+++ rpc-ssl-patch.sh
@@ -16,9 +16,14 @@ fi
 knife ssh "role:*controller*" "apt-get update; apt-get install libssl1.0.0 ssl-cert; service apache2 restart"
 
 #add cert override to environment
-knife exec -E '@e=Chef::Environment.load("rpcs"); a=@e.override_attributes; a.merge!({"horizon" => {"ssl" => {"key_override" => "/etc/apache2/ssl/server.key", "cert_override" => "/etc/apache2/ssl/server.crt"}}}); @e.override_attributes(a); @e.save'
-
-echo "added SSL override to environment, running chef-client on controllers"
+HASKEY=unset
+if ! knife environment show rpcs | grep key_override >/dev/null; then
+  knife exec -E '@e=Chef::Environment.load("rpcs"); a=@e.override_attributes; a.merge!({"horizon" => {"ssl" => {"key_override" => "/etc/apache2/ssl/server.key", "cert_override" => "/etc/apache2/ssl/server.crt"}}}); @e.override_attributes(a); @e.save'
+  echo "added SSL override to environment, running chef-client on controllers"
+else
+  echo 'SSL override already exists! Not overriding.'
+  HASKEY=yes
+fi
 
 sleep 4
 
@@ -38,7 +43,11 @@ echo "Patching compute nodes"
 
 sleep 2
 #update compute nodes
-knife ssh "role:single-compute" "apt-get update; apt-get install -y libssl1.0.0 ssl-cert; if [ -a /etc/init.d/neutron-plugin-openvswitch-agent ]; then service neutron-plugin-openvswitch-agent restart; else service quantum-plugin-openvswitch-agent restart; fi;  service monit restart"
+knife ssh "role:single-compute" "apt-get update; apt-get install -y libssl1.0.0 ssl-cert; if pgrep neutron >/dev/null; then service neutron-plugin-openvswitch-agent restart; elif pgrep quantum >/dev/null; then service quantum-plugin-openvswitch-agent restart; else echo 'Not running neutron/quantum'; fi; service monit restart"
 
 
 echo "UPDATED Please verify everything still works"
+
+if [ $HASKEY == "yes" ]; then
+  echo -e "\033[1m***IMPORTANT*** Customer had an override set for Horizon's SSL keys. Please notify the customer that their custom cert needs to be renewed.\033[0m"
+fi
